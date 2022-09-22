093630aaa5093f5b85e8fc8d0faed5a3b4b59434
Kevin <Kevin@DESKTOP-6MPDRDE>
2022-09-22 09:04:27 -0700

    Add project files.

---
+++ Activity 4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Activity_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.secondsBox = new System.Windows.Forms.TextBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter number of seconds:";
+            // 
+            // secondsBox
+            // 
+            this.secondsBox.Location = new System.Drawing.Point(12, 43);
+            this.secondsBox.Name = "secondsBox";
+            this.secondsBox.Size = new System.Drawing.Size(271, 22);
+            this.secondsBox.TabIndex = 0;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(289, 42);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 1;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 84);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.secondsBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Seconds Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox secondsBox;
+        private System.Windows.Forms.Button ok;
+    }
+}
+
---
+++ Activity 4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            int seconds;
+            if(int.TryParse(secondsBox.Text, out seconds)) 
+            {
+                if(seconds >= 86400)
+                {
+                    MessageBox.Show(seconds / 86400 + " day(s).");
+                } else if (seconds >= 3600)
+                {
+                    MessageBox.Show(seconds / 3600 + " hour(s).");
+                }
+                else if (seconds >= 60)
+                {
+                    MessageBox.Show(seconds / 60 + " minute(s).");
+                } else
+                {
+                    MessageBox.Show(seconds + " second(s).");
+                }
+            } else 
+            {
+                MessageBox.Show("Enter a valid number.");
+            }
+        }
+    }
+}
